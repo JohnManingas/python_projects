c1b2d3d08e2cbab9547b311debcea4c7f182ca6e
JohnManingas <john.d.maningas@gmail.com>
2024-08-30 19:10:41 -0400

    Update pizza-order-generator.py
    cleaned up code for more readability

--- pizza-order-generator.py
+++ pizza-order-generator.py
@@ -1,33 +1,42 @@
-# This project helps you order a pizza!
+#Let's order some pizza
 
-# User input for their pizza order.
-print("Welcome to Python Pizza Deliveries!")
-size=input("What size pizza would you like? S M or L?: ")
-pepperoni=input("Would you like pepperoni? Y or N: ")
-extra_cheese=input("Would you like extra cheese? Y or N: ")
+#Greet our guests!
+print("Welcome to Marky's pizza!")
+
+#Retrieve input from our customers
+#automatically translates user input into lowercase
+size=input("What size pizza would you like? 'S' for Small, 'M' for Medium, 'L' for Large'\n").lower()
+pepperoni=input("Would you like pepperoni? 'Y' for yes, 'N' for no\n").lower()
+cheese=input("Would you like extra cheese? 'Y' for yes, 'N' for no\n").lower()
+
+#Initiate a variable for the bill
 bill = 0
 
-
-# Work out the size of the pizza
-if size == "S":
+#Calculate the amount for the size of the pizza
+if size == 's':
     bill += 15
-elif size == "M":
+elif size == 'm':
     bill += 20
-elif size == "L":
+elif size == 'l':
     bill += 25
 else:
-    print("Please input a proper pizza size.")
+    print("Please choose a proper size for the pizza")
 
-# Work out if they want pepperoni or not
-if pepperoni == "Y":
-    if size == "S":
+# Include the price of cheese
+# Price of cheese depends on size of pizza
+if cheese == 'y':
+    if size == 's':
+        bill += 1
+    elif size == 'm' or size == 'l':
         bill += 2
-    else:
+
+#Find if they want pepperoni
+#Pepperoni depends on the size of the pizza
+if pepperoni == 'y':
+    if size == 's':
+        bill += 2
+    elif size == 'm' or size == 'l':
         bill += 3
 
-# Work out if they want extra cheese or not
-if extra_cheese == "Y":
-    bill += 1
-
-# Output the bill total
-print(f"Your final bill is: ${bill}.")
+# Calculate the total amount for the bill
+print(f"Your final bill is ${bill}.")
